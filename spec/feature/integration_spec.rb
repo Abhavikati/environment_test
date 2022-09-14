047fff0382e7b89622c8c7f6e775ed4f9479b919
root <root@localhost>
2022-09-14 04:11:06 +0000

    final

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -1,42 +1,27 @@
 # location: spec/feature/integration_spec.rb
 require 'rails_helper'
 
-RSpec.describe 'Creating a book', type: :feature do
-  scenario 'valid inputs' do
-    visit new_book_path
-    fill_in 'Title', with: 'harry potter'
-    click_on 'Add Book'
-    visit books_path
-    expect(page).to have_content('harry potter')
+def select_date(date, options = {})
+    field = options[:from]
+    base_id = find(:xpath, ".//label[contains(.,'#{field}')]")[:for]
+    year, month, day = date.split(',')
+    select year,  :from => "#{base_id}_1i"
+    select month, :from => "#{base_id}_2i"
+    select day,   :from => "#{base_id}_3i"
   end
-end
 
-RSpec.describe 'Creating a author', type: :feature do
-  scenario 'valid inputs' do
-    visit new_book_path
-    fill_in 'Author', with: 'J.K. Rowling'
-    click_on 'Add Author'
-    visit books_path
-    expect(page).to have_content('J.K. Rowling')
-  end
-end
-
-RSpec.describe 'Creating a price', type: :feature do
-  scenario 'valid inputs' do
-    visit new_book_path
-    fill_in 'Price', with: '14.99'
-    click_on 'Add Price'
-    visit books_path
-    expect(page).to have_content('14.99')
-  end
-end
-
-RSpec.describe 'Creating a published date', type: :feature do
-  scenario 'valid inputs' do
-    visit new_book_path
-    fill_in 'Published Date', with: '04/02/1998'
-    click_on 'Add Published Date'
-    visit books_path
-    expect(page).to have_content('04/02/1998')
-  end
+  RSpec.describe 'Creating a book', type: :feature do
+    scenario 'valid inputs' do
+      visit new_book_path
+      fill_in 'Title', with: 'The Fellowship of the Ring'
+      fill_in 'Author', with: 'J. R. R. Tolkien'
+      fill_in 'Price', with: '9.99'
+      select_date("1954,July,29", :from => "Published")
+      click_on 'Create Book'
+      visit books_path
+      expect(page).to have_content('The Fellowship of the Ring')
+      expect(page).to have_content('J. R. R. Tolkien')
+      expect(page).to have_content('9.99')
+      expect(page).to have_content('1954-07-29')
+    end
 end
